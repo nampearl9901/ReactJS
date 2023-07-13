128088259f7f2b5e1f6b71131e421d359943c453
nampearl9901 <nampearl9901@gmail.com>
2023-07-13 11:38:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Baitap/Nav';
+import Header from './Baitap/Header';
+import Second from './Baitap/Second';
+import Footer from './Baitap/Footer';
+
+function App() {
+  return (
+    <div className="App">
+     <Nav/>
+     <Header/>
+     <Second/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
